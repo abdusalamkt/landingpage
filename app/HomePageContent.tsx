ae446c010435106cbf11008832709a18caa06b66
abdusalamkt <abdusalamgibca@gmail.com>
2025-09-15 14:11:12 +0400

    preload-removed

--- app/HomePageContent.tsx
+++ app/HomePageContent.tsx
@@ -1,6 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
-import SplashScreen from './components/SplashScreen';
+import { useEffect } from 'react';
 // import Header from './components/Header';
 import Banner from './components/Banner';
 import Stats from './components/Stats';
@@ -28,35 +27,6 @@ type Product = {
 };
 
 export default function HomePageContent({ fields }: { fields: any }) {
-  const [showSplash, setShowSplash] = useState(true);
-  const [pageLoaded, setPageLoaded] = useState(false);
-
-  useEffect(() => {
-    function handlePageShow(event: PageTransitionEvent) {
-      if (event.persisted) {
-        setShowSplash(false);
-      } else {
-        setShowSplash(true);
-      }
-    }
-
-    window.addEventListener('pageshow', handlePageShow);
-
-    const navEntry = performance.getEntriesByType('navigation')[0] as PerformanceNavigationTiming | undefined;
-    if (navEntry?.type === 'back_forward') {
-      setShowSplash(false);
-    } else {
-      setShowSplash(true);
-    }
-
-    return () => window.removeEventListener('pageshow', handlePageShow);
-  }, []);
-
-  const onSplashFinish = () => {
-    setShowSplash(false);
-    setPageLoaded(true);
-  };
-
   useEffect(() => {
     if (typeof window !== 'undefined') {
       window.history.scrollRestoration = 'manual';
@@ -91,76 +61,67 @@ export default function HomePageContent({ fields }: { fields: any }) {
   }));
 
   return (
-    <>
-      {showSplash && <SplashScreen onFinish={onSplashFinish} />}
+    <main>
+      {/* <Header /> */}
+      <FloatingSidebar />
 
-      <main style={{ visibility: showSplash ? 'hidden' : 'visible' }}>
-        {/* <Header /> */}
-        <FloatingSidebar />
+      <AnimatedSection animation="fadeUp" delay={0.2}>
+        <Banner heading={fields.bannerHeading} images={fields.bannerImages} />
+      </AnimatedSection>
 
-        <AnimatedSection animation="fadeUp" delay={0.2}>
-          <Banner
-            heading={fields.bannerHeading}
-            images={fields.bannerImages}
-          />
-        </AnimatedSection>
+      {/* Uncomment if you want to use NewsBanner */}
+      {/* <AnimatedSection animation="fadeUp" delay={0.4}>
+        <NewsBanner
+          heading={fields.newsHeading}
+          image={{
+            sourceUrl: fields.newsBannerImage?.sourceUrl || '',
+            altText: fields.newsBannerImage?.altText || '',
+          }}
+          slug={fields.newsPost?.slug}
+        />
+      </AnimatedSection> */}
 
-        {/* Uncomment if you want to use NewsBanner */}
-        {/* <AnimatedSection animation="fadeUp" delay={0.4}>
-          <NewsBanner
-            heading={fields.newsHeading}
-            image={{
-              sourceUrl: fields.newsBannerImage?.sourceUrl || '',
-              altText: fields.newsBannerImage?.altText || '',
-            }}
-            slug={fields.newsPost?.slug}
-          />
-        </AnimatedSection> */}
-        <NewsNav
-  heading={fields.newsHeading || 'Latest Update'}
-  image={{
-    sourceUrl: fields.newsBannerImage?.sourceUrl || '',
-    altText: fields.newsBannerImage?.altText || '',
-  }}
-  url={fields.newsUrl || '#'}
-/>
+      <NewsNav
+        heading={fields.newsHeading || 'Latest Update'}
+        image={{
+          sourceUrl: fields.newsBannerImage?.sourceUrl || '',
+          altText: fields.newsBannerImage?.altText || '',
+        }}
+        url={fields.newsUrl || '#'}
+      />
 
+      <AnimatedSection animation="fadeUp" delay={0.6}>
+        <Stats
+          experience={fields.yearsOfExperience}
+          clients={fields.numberOfClients}
+          totalworkforce={fields.workforce}
+          totalprojects={fields.projectsCompleted}
+        />
+      </AnimatedSection>
 
+      <AnimatedSection animation="slideRight" delay={0.1}>
+        <Products description={fields.productsDescription} products={products} />
+      </AnimatedSection>
 
-        <AnimatedSection animation="fadeUp" delay={0.6}>
-          <Stats
-            experience={fields.yearsOfExperience}
-            clients={fields.numberOfClients}
-            totalworkforce={fields.workforce}
-            totalprojects={fields.projectsCompleted}
-            key={pageLoaded ? 'loaded' : 'loading'}
-          />
-        </AnimatedSection>
+      <AnimatedSection animation="fadeUp" delay={0.6}>
+        <ImageLayout data={{ page: { landingPageFields: fields } }} />
+      </AnimatedSection>
 
-        <AnimatedSection animation="slideRight" delay={0.1}>
-          <Products description={fields.productsDescription} products={products} />
-        </AnimatedSection>
+      <AnimatedSection animation="slideRight" delay={0.3}>
+        <ResourcesSection />
+      </AnimatedSection>
 
-        <AnimatedSection animation="fadeUp" delay={0.6}>
-          <ImageLayout data={{ page: { landingPageFields: fields } }} />
-        </AnimatedSection>
+      <AnimatedSection animation="fadeUp" delay={0.4}>
+        <OurClientsSection logos={fields.clientLogos || []} />
+      </AnimatedSection>
 
-        <AnimatedSection animation="slideRight" delay={0.3}>
-          <ResourcesSection />
-        </AnimatedSection>
+      {/* <AnimatedSection animation="fadeUp" delay={0.5}>
+        <TestimonialSlider testimonials={fields.testimonials || []} />
+      </AnimatedSection> */}
 
-        <AnimatedSection animation="fadeUp" delay={0.4}>
-          <OurClientsSection logos={fields.clientLogos || []} />
-        </AnimatedSection>
+      <GetInTouchSection />
 
-        {/* <AnimatedSection animation="fadeUp" delay={0.5}>
-  <TestimonialSlider testimonials={fields.testimonials || []} />
-</AnimatedSection> */}
-
-        <GetInTouchSection />
-
-        {/* <ContactUs /> */}
-      </main>
-    </>
+      {/* <ContactUs /> */}
+    </main>
   );
 }
--- app/components/ResourcesSection.js
+++ app/components/ResourcesSection.js
@@ -7,11 +7,12 @@ import './ResourcesSection.css';
 const ResourcesSection = () => {
   return (
     <div className="resources-wrapper">
-      <h2 className="resources-heading">RESOURCES</h2>
+     
+      <div className="resources-section">
+         <h2 className="resources-heading">RESOURCES</h2>
       <p className="resources-description">
 A lot happens at Gibca Furniture, and you would not want to miss out! Be up to date with the latest trends, our current projects and get your hands on the latest technical information on our products - all under the resources section!       </p>
 
-      <div className="resources-section">
         <Link href="/case-study" className="resource-item">Case Study</Link>
         <div className="divider" />
         <Link href="/downloads" className="resource-item">Downloads</Link>
